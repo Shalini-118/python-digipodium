861bde2faabc5bfcaf9fd2c68c7473fe72f08951
Shalini-118 <108971017+Shalini-118@users.noreply.github.com>
2022-10-01 12:24:49 +0530

    data_analysis3

--- data science/flipkart_spider.py
+++ data science/flipkart_spider.py
@@ -1,16 +1,37 @@
 from dputils import scrape
+import pandas as pd
 
 #step1. get the data at soup object
 #step2. create the setup dictionary
 #step3. pass the dictionary into the extract_many( ) function
 #step4. repeat the step 1 to 3 util data keep coming
 # step5. check and save data into file
+def getdata(q):
+    t={'tag':'div','attrs':{'class':'_1YokD2 _3Mn1Gg'}}
+    rep_items={'tag':'div','attrs':{'class':'_1AtVbE col-12-12'}}
+    titles={'tag':'div','attrs':{'class':'_4rR01T'}}
+    price={'tag':'div','attrs':{'class':'_30jeq3 _1_WHN1'}}
+    link={'tag':'a','attrs':{'class':'_1fQZEK'},'output':'href'}
 
-# understanding the URL
-url='https://www.flipkart.com/search?q=laptop&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=of'
-print(url.split('?')[-1].split('&'))
+    pos=1    
+    all_data=[]
+    while True:
+    # understanding the URL
+        url='https://www.flipkart.com/search?q={q}&page={pos}'
+        print(url)
+        soup=scrape.get_webpage_data(url)
+        data=scrape.extract_many(soup,target=t,items=rep_items,title=t)
+        if isinstance(data,list):
+            if len(data)>0:
+                pos+=1
+                all_data.extend(data)
+            else:
+                break
+        else:
+            break
+    return all_data    
+#use
+laptops=getdata('laptops')
+pd.DataFrame(laptops).to_csv('laptop_data.csv')       
 
-#step1
-query='laptop'
-pos=1
-url=f'https://www.flipkart.com/search?q={query}&page={pos}'
+

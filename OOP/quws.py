2684efc0d1f564d1d757b7b2238b3b738cf85344
Shalini-118 <108971017+Shalini-118@users.noreply.github.com>
2022-10-22 12:55:04 +0530

    image_processing

---
+++ OOP/quws.py
@@ -0,0 +1,4 @@
+class stud:
+    def__init__(self,roll_no,grade):
+    self.roll_no
+    
---
+++ app_one/app.py
@@ -0,0 +1,6 @@
+'''
+to run do not click play button but instead in terminal
+type cd app_one
+ streamlit run app.py
+'''
+import streamlit as st
